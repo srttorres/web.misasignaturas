01813ec5fa2a3cb08ac27e8a373962edce95ef4b
root <root@localhost.localdomain>
2018-02-24 16:05:09 +0100

    first version

---
+++ misasignaturas/index.php
@@ -0,0 +1,26 @@
+<?php 
+//PAGINA PRINCIPAL INCLUYE DOS BLOQUES:
+//bloque_nuevoAlumno
+//bloque_listadoAlumnos
+$TITULO_PAGINA="ASIGNATURA";
+$TITULO_BLOQUE='TITULO_BLOQUE=null';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	
+ 	<meta charset="utf-8">
+  	<title><?php echo "$TITULO_PAGINA"; ?></title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+<h1><?php echo "$TITULO_PAGINA"; ?></h1>
+
+
+
+
+ <footer>
+  <p>designed by srttorres</p>
+</footer> 
+</body>
+</html> 
